ea8fad9daac833ab7b98613bb2b816e8f0162b12
Jon Daniel <joneqdaniel@gmail.com>
2026-04-21 19:04:27 +0200

    update

--- README.md
+++ README.md
@@ -7,14 +7,22 @@ CFLAGS="-march=<your architecture> -mfpmath=<your SIMD>" PREFIX="your destinatio
 ```
 ## Usage
 ```sh
-Usage: conaspect <width> <height> [scale]...
+Usage: conaspect <width> <height> [col scale] [row scale]...
 Calculate aspect ratio and kernel console parameters.
 
   <width>  - integer screen width
   <height> - integer screen height
-  [scale]  - optional floating point row multiplier
+  [col scale]  - optional floating point column multiplier
+  [row scale]  - optional floating point row multiplier
 
 Examples:
-conaspect 1024 768
-conaspect 1024 768 2.0
+conaspect 1920 1080
+conaspect 1920 1080 2.0
+conaspect 1920 1080 1.0 2.0
+```
+## Output
+```sh
+MODE: 1920x1080 16:9 CONSOLE: 240x67 SCALE: 1.000000x1.000000
+MODE: 1920x1080 16:9 CONSOLE: 480x67 SCALE: 2.000000x1.000000
+MODE: 1920x1080 16:9 CONSOLE: 240x134 SCALE: 1.000000x2.000000
 ```
--- conaspect.c
+++ conaspect.c
@@ -14,20 +14,24 @@
 #define REGPARM
 #endif
 
-#ifndef __MMX__
 #ifdef __clang__
-typedef int32_t __m64 __attribute__((ext_vector_type(2), __may_alias__));
+#define vec(t,n) typeof(t __attribute__((ext_vector_type(n), __may_alias__)))
+typedef vec(int32_t,2) vec2i;
+typedef vec(float,2) vec2f;
+#elif __GNUC__
+#define vec(t,n) typeof(t __attribute__((vector_size(sizeof(t) * n), __may_alias__)))
+typedef vec(int32_t,2) vec2i;
+typedef vec(float,2) vec2f;
+#elif _MSC_VER
+#include <intrin.h>
+typedef __m64 vec2i;
+typedef __m128 vec2f;
 #else
-typedef int32_t __m64 __attribute__((vector_size(sizeof(int32_t) * 2), __may_alias__));
-#endif
-#else
-#include <mmintrin.h>
+#warning "Your compiler doens't support SIMD vector extensions!"
 #endif
 
-typedef __m64 vec2i;
 
 INLINE REGPARM static int gcd(int x, int y) { return y == 0 ? MAX(x, -x) : gcd(y, x % y); }
-
 
 int main(int argc, char** argv)
 {
@@ -37,14 +41,15 @@ int main(int argc, char** argv)
 		vec2i ratio   = { gcd(res[0], res[1]), gcd(res[0], res[1]) }; 
 		vec2i aspect  = { res[0] / ratio[0]  , res[1] / ratio[1]   };
 		vec2i console = { (res[0] >> 3)      , (res[1] >> 4)       };
+		vec2f scale   = { argc > 3 ? strtof(argv[3], NULL) : 1.0f,
+		                  argc > 4 ? strtof(argv[4], NULL) : 1.0f  };
 
-		if(argc > 3)
-			console[1] *= strtof(argv[3], NULL);
+		console = __builtin_convertvector((vec(float,2)){ (float)console[0], (float)console[1] } * scale, vec2i);
 
-		fprintf(stdout, "MODE: %dx%d %d:%d CONSOLE: %dx%d\n",
+		fprintf(stdout, "MODE: %dx%d %d:%d CONSOLE: %dx%d SCALE: %fx%f\n",
 		                res[0], res[1],
 				aspect[0], aspect[1],
-				console[0], console[1]);
+				console[0], console[1], scale[0], scale[1]);
 
 		exit(EXIT_SUCCESS);
 	}
